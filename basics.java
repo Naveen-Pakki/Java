3555b073fb29ea63744ac1f1a2dbfe9d4951fa4e
Naveen-Pakki <pakkinaveen009@gmail.com>
2025-08-07 12:22:48 +0000

    updation

--- basics.java
+++ basics.java
@@ -3,3 +3,13 @@ public class basics {
         System.out.println(2+5);
     }
 }
+
+// This is a simple Java program that prints the sum of 2 and 5 to the console.
+// It demonstrates basic syntax and structure of a Java application.
+// To run this program, you can use a Java IDE or compile it using the command line
+// with `javac basics.java` followed by `java basics`.
+// Make sure you have Java Development Kit (JDK) installed on your system to compile and
+// run Java programs.
+// The output of this program will be:
+// 7
+
